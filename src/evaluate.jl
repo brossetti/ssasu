5fc6746107e74fc7c7f3dbb6ee63a80a964df967
brossetti <brossetti@nsintheta.com>
2019-01-12 18:24:18 -0500

    added eval for endmember estimation

--- src/evaluate.jl
+++ src/evaluate.jl
@@ -18,6 +18,11 @@ function prop_ind(W)
     return sqrt(sum((WW - D).^2))/sqrt(sum(D.^2))
 end
 
+function sad(X,Y)
+    denom = dropdims(sqrt.(sum(X.^2,dims=1)).*sqrt.(sum(Y.^2,dims=1)),dims=1)
+    return acos.(diag(transpose(X)*Y)./denom)
+end
+
 function run()
     exp_filenames = ["E-TDFH2-1",
                      "E-TDFH2-2",
@@ -33,7 +38,7 @@ function run()
     reconstruction_error = zeros(Float32,(nimgs,2))
     proportion_indeterminacy = zeros(Float32,(nimgs,2))
 
-    # evaluate
+    # evaluate unmixing
     S_nls = readdlm(joinpath("..","results","mean-estimated-endmembers.csv"), ',', Float32)
     for i = 1:nimgs
         # load original image
@@ -52,21 +57,35 @@ function run()
         reconstruction_error[i,1] = rre(Y,Y_hat)
         proportion_indeterminacy[i,1] = prop_ind(W)
 
-        # evaluate SANMF
-        S_sanmf = readdlm(joinpath("..","results","sanmf",string(exp_filenames[i],"-sanmf-S.csv")), ',', Float32)
-        b = readdlm(joinpath("..","results","sanmf",string(exp_filenames[i],"-sanmf-b.csv")), ',', Float32)
-        unmixed_filename = joinpath("..","results","sanmf",string(exp_filenames[i], "-sanmf.tif"))
+        # evaluate SSASU
+        S_ssasu = readdlm(joinpath("..","results","ssasu",string(exp_filenames[i],"-ssasu-S.csv")), ',', Float32)
+        b = readdlm(joinpath("..","results","ssasu",string(exp_filenames[i],"-ssasu-b.csv")), ',', Float32)
+        unmixed_filename = joinpath("..","results","ssasu",string(exp_filenames[i], "-ssasu.tif"))
         img = convert(Array{Float32},load(unmixed_filename))
         N = size(img,3)
         W = transpose(reshape(img,(P,N)))
-        Y_hat = S_sanmf*W .+ b
+        Y_hat = S_ssasu*W .+ b
         reconstruction_error[i,2] = rre(Y,Y_hat)
         proportion_indeterminacy[i,2] = prop_ind(W)
-
     end
 
     writedlm(joinpath("..","results","reconstruction-error.csv"), reconstruction_error, ",")
     writedlm(joinpath("..","results","proportion-indeterminacy.csv"), proportion_indeterminacy, ",")
+
+    # evaluate endmember estimation
+    S_true = readdlm(joinpath("..","data","ref","fluorometer-endmembers.csv"),',',Float32;header=true)[1]
+    S_anmf = readdlm(joinpath("..","results","anmf-estimated-endmembers.csv"), ',', Float32)
+
+    S_true = S_true[:,2:end]
+    S_nls = S_nls[:,2:end]
+    S_anmf = S_anmf[:,2:end]
+
+    spectral_angle = zeros(Float32,(size(S_true,2),2))
+    spectral_angle[:,1] = sad(S_true,S_nls)
+    spectral_angle[:,2] = sad(S_true,S_anmf)
+
+    writedlm(joinpath("..","results","spectral-angle.csv"), spectral_angle, ",")
+
 end
 
 end  # module Evaluate
